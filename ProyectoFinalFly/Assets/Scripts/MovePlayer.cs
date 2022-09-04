2de0452663a4a031db364d06e37013db181f448c
PepeGitHub1 <110578123+PepeGitHub1@users.noreply.github.com>
2022-09-04 15:38:54 -0500

    Actualizacion

--- ProyectoFinalFly/Assets/Scripts/MovePlayer.cs
+++ ProyectoFinalFly/Assets/Scripts/MovePlayer.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-
     private float rotationAxisX = 0f;
-
     private float horizontalInput;
     private float verticalInput;
     [SerializeField][Range(5f, 10f)] private float speed = 5f;
@@ -24,6 +22,19 @@ public class MovePlayer : MonoBehaviour
     {
         MovementPlayer();
         RotatePlayer();
+
+    }
+
+
+
+    private void MovementPlayer()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
+
     }
 
     public void RotatePlayer()
@@ -34,14 +45,5 @@ public class MovePlayer : MonoBehaviour
 
         Quaternion newRotation = Quaternion.Euler(0, 0, rotationAxisX);
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 10f * Time.deltaTime);
-    }
-
-    private void MovementPlayer()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * speed * Time.deltaTime);
     }
 }
